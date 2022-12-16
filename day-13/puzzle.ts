de033f3593735af8ef13088b71e8484469e75753
Kory Porter <kory.porter@eucalyptus.vc>
2022-12-16 18:34:06 +1100

    day 13 puzzle 2

--- day-13/puzzle.ts
+++ day-13/puzzle.ts
@@ -4,10 +4,6 @@ function inOrder(
   left: number | Array<number>,
   right: number | Array<number>
 ): null | boolean {
-  // If both values are integers, the lower integer should come first.
-  // If the left integer is lower than the right integer, the inputs are in the right order.
-  // If the left integer is higher than the right integer, the inputs are not in the right order.
-  // Otherwise, the inputs are the same integer; continue checking the next part of the input.
   if (Number.isInteger(left) && Number.isInteger(right)) {
     if (left === right) {
       return null;
@@ -15,10 +11,6 @@ function inOrder(
     return left < right;
   }
 
-  // If the left list runs out of items first, the inputs are in the right order.
-  // If the right list runs out of items first, the inputs are not in the right order.
-  // If both values are lists, compare the first value of each list, then the second value, and so on.
-  // If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
   if (Array.isArray(left) && Array.isArray(right)) {
     for (let i = 0; i < Math.max(left.length, right.length); i += 1) {
       if (typeof left[i] === "undefined") {
@@ -39,8 +31,6 @@ function inOrder(
     return null;
   }
 
-  // If exactly one value is an integer, convert the integer to a list which contains that integer as its only value,then retry the comparison.
-
   if (Array.isArray(left) && Number.isInteger(right)) {
     return inOrder(left, [right as number]);
   }
@@ -51,11 +41,26 @@ function inOrder(
   throw new Error("something gone real bad");
 }
 
-const result = input
+const parsed = input
   .split("\n\n")
   .map((pair) => pair.split("\n"))
-  .map((p) => [eval(p[0]), eval(p[1])])
+  .map((p) => [eval(p[0]), eval(p[1])]);
+
+const puzzle1 = parsed
   .map((p, i) => (inOrder(p[0], p[1]) ? i + 1 : 0))
   .reduce((sum, index) => sum + index, 0);
 
-console.log("Puzzle 1 = ", result);
+const puzzle2 = [...parsed, [[[2]], [[6]]]]
+  .flatMap((p) => p)
+  .sort((a, b) => {
+    const res = inOrder(a, b);
+    if (res === null) {
+      return 0;
+    }
+    return res ? -1 : 1;
+  })
+  .map((v, i) => (["[[2]]", "[[6]]"].includes(JSON.stringify(v)) ? i + 1 : 1))
+  .reduce((sum, index) => sum * index, 1);
+
+console.log("Puzzle 1 = ", puzzle1);
+console.log("Puzzle 2 = ", puzzle2);
